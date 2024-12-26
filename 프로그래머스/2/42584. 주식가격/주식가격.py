63bd0506629e604b235602057f9753a66beb7acc
yesolee <58902084+yesolee@users.noreply.github.com>
2024-12-26 10:55:00 +0900

    [level 2] Title: 주식가격, Time: 27.24 ms, Memory: 19.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,18 +1,36 @@
-from collections import deque
+# from collections import deque
+
+# def solution(prices):
+#     prices = deque(prices)
+#     answer= []
+#     while prices:
+#         item = prices.popleft()
+#         count = 0
+#         if not prices:
+#             answer.append(0)
+#             break
+
+#         for i in prices :
+#             count += 1
+#             if item > i:
+#                 break
+#         answer.append(count)
+#     return answer
 
 def solution(prices):
-    prices = deque(prices)
-    answer= []
-    while prices:
-        item = prices.popleft()
-        count = 0
-        if not prices:
-            answer.append(0)
-            break
+    answer = [0] * len(prices)  # 결과를 저장할 리스트 초기화
+    stack = []  # 스택 초기화
 
-        for i in prices :
-            count += 1
-            if item > i:
-                break
-        answer.append(count)
+    for i in range(len(prices)):
+        # 현재 가격이 스택에 저장된 가격보다 작으면
+        while stack and prices[i] < prices[stack[-1]]:
+            idx = stack.pop()  # 스택에서 인덱스를 꺼냄
+            answer[idx] = i - idx  # 현재 인덱스와 스택 인덱스의 차이를 저장
+        stack.append(i)  # 현재 인덱스를 스택에 추가
+
+    # 스택에 남아 있는 인덱스 처리
+    while stack:
+        idx = stack.pop()
+        answer[idx] = len(prices) - 1 - idx
+
     return answer
