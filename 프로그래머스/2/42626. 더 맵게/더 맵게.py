060ca6fc82399263c57b782d57d77f356fdf18c5
yesolee <58902084+yesolee@users.noreply.github.com>
2024-12-27 23:48:42 +0900

    [level 2] Title: 더 맵게, Time: 1635.65 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,18 +1,56 @@
-import heapq
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+import heapq as hq
 
 def solution(scoville, K):
-    heapq.heapify(scoville)
-    count = 0
-    while scoville:
-        first = heapq.heappop(scoville)
+
+    hq.heapify(scoville)
+    answer = 0
+    while True:
+        first = hq.heappop(scoville)
         if first >= K:
-            return count
-        if not scoville:
+            break
+        if len(scoville) == 0:
             return -1
-        count += 1
-        second = heapq.heappop(scoville)
-        new = first + second * 2
-        heapq.heappush(scoville, new)
+        second = hq.heappop(scoville)
+        hq.heappush(scoville, first + second*2)
+        answer += 1  
+
+    return answer
+
+# 메모리: 51.8 MB, 시간: 1585.97 ms
+
+# import heapq
+
+# def solution(scoville, K):
+#     heapq.heapify(scoville)
+#     count = 0
+#     while scoville:
+#         first = heapq.heappop(scoville)
+#         if first >= K:
+#             return count
+#         if not scoville:
+#             return -1
+#         count += 1
+#         second = heapq.heappop(scoville)
+#         new = first + second * 2
+#         heapq.heappush(scoville, new)
 
 # 메모리: 51.8 MB, 시간: 1613.05 ms
 # import heapq
