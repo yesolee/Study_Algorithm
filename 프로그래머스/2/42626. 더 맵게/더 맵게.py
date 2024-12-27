365e616e1d825b0bcaa71f8308040e8ec3367d90
yesolee <58902084+yesolee@users.noreply.github.com>
2024-12-27 23:47:20 +0900

    [level 2] Title: 더 맵게, Time: 1585.97 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -7,10 +7,27 @@ def solution(scoville, K):
         first = heapq.heappop(scoville)
         if first >= K:
             return count
-        elif scoville and first < K:
-            count += 1
-            second = heapq.heappop(scoville)
-            new = first + second * 2
-            heapq.heappush(scoville, new)
-        else :
+        if not scoville:
             return -1
+        count += 1
+        second = heapq.heappop(scoville)
+        new = first + second * 2
+        heapq.heappush(scoville, new)
+
+# 메모리: 51.8 MB, 시간: 1613.05 ms
+# import heapq
+
+# def solution(scoville, K):
+#     heapq.heapify(scoville)
+#     count = 0
+#     while scoville:
+#         first = heapq.heappop(scoville)
+#         if first >= K:
+#             return count
+#         elif scoville and first < K:
+#             count += 1
+#             second = heapq.heappop(scoville)
+#             new = first + second * 2
+#             heapq.heappush(scoville, new)
+#         else :
+#             return -1
